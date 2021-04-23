a93666109e39638541fe153d3d5f69f6e0054e98
Batur Arslan <arslanbaturyilmaz@gmail.com>
2021-04-23 21:01:34 +0300

    javascript-odev2 eklendi

---
+++ javascript-patika/javascript-odev2/js/script.js
@@ -0,0 +1,62 @@
+const input = document.getElementById("task");
+const buttonAdd = document.getElementById("liveToastBtn");
+const listParent = document.getElementById("list");
+
+const tasks = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
+if (tasks.length !== 0) updateTaskList();
+
+function addTask(task) {
+  tasks.push({ content: task, checked: false });
+  updateTaskList();
+}
+
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  updateTaskList();
+}
+
+function updateChecked(index) {
+  tasks[index].checked = !tasks[index].checked;
+  updateTaskList();
+}
+
+function updateTaskList() {
+  listParent.innerHTML = tasks
+    .map(
+      (task) =>
+        `<li class=${task.checked ? "checked" : ""}>${task.content}
+          <span class="close">×</span>
+         </li>`
+    )
+    .join("");
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// EVENTS
+
+function handleListParent(e) {
+  const getIndex = (item) => Array.from(listParent.children).indexOf(item);
+
+  if (e.target.tagName === "LI") {
+    updateChecked(getIndex(e.target));
+  } else {
+    deleteTask(getIndex(e.target.parentElement));
+  }
+}
+
+function handleButtonAdd() {
+  const task = input.value.trim();
+  input.value = "";
+
+  if (!task) {
+    $(".error").toast("show");
+    return;
+  }
+
+  addTask(task);
+  $(".success").toast("show");
+}
+
+listParent.addEventListener("click", handleListParent);
+buttonAdd.addEventListener("click", handleButtonAdd);
